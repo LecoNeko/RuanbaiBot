a320a91246e56ada6a0011d925c42698d8563d2c
RBpencil <1320265781@qq.com>
2022-02-11 23:20:49 +0800

    增加了saucenao搜本子功能
    add a function of search tojin on saucenao

--- plugins/saunao.py
+++ plugins/saunao.py
@@ -3,7 +3,23 @@ import aiocqhttp
 from nonebot import CommandSession, MessageSegment
 import nonebot
 import re
+import json
 from plugins.utils import *
+
+APIKEY = 'f2cddb0d913ab5135c6f848e1fe0b8c1a2a61ded'
+
+SAUCEURL = 'https://saucenao.com/search.php?\
+api_key=f2cddb0d913ab5135c6f848e1fe0b8c1a2a61ded\
+&output_type=2&\
+testmode=1&\
+dbmaski=32768&\
+db=5&\
+numres=5&\
+url='
+
+'''
+https://saucenao.com/search.php?apikey=f2cddb0d913ab5135c6f848e1fe0b8c1a2a61ded&output_type=2&testmode=1&dbmaski=32768&db=5&numres=5&url=https://gchat.qpic.cn/gchatpic_new/1320265781/3967125332-3219453593-6C581F8F95E9E20AD31D2CB632FA08C7/0?term=3,subType=0
+'''
 
 bot = nonebot.get_bot()
 
@@ -11,16 +27,33 @@ bot = nonebot.get_bot()
 async def _(event):
     info = await bot.get_login_info()
     id = info['user_id']
-    if '[CQ:at,qq=' + str(id) +']' in event['raw_message'] and '[CQ:image' in event['raw_message']:
+    if ('[CQ:at,qq=' + str(id) +']' in event['raw_message'] or event['message_type'] == 'private')\
+         and '[CQ:image' in event['raw_message']:
+
         await bot.send(event, message='正在搜索。。。')
         CQimg = getCQimgInMesg(str(event['message']))
         url = getImgUrlInCQ(CQimg)
-        imginfo = saucenaoSearch(url)
+        imginfo = saucenaoSearch(url, SAUCEURL)
         for iter in imginfo:
-            simp = iter['simp']
-            pid = iter['pid']
-            author = iter['author']
-            img = setuMesg(iter['url'])
-            ans = '作者：' + author + 'pixiv id：' + pid + '相似度：' +simp
-            await bot.send(event, ans)
-            await bot.send(event, img)
+            simp = iter['similarity']
+            thum = iter['thumbnail']
+            if iter['type'] == 'doujin':
+                eng_name = iter['eng_name']
+                jp_name = iter['jp_name']
+                creator = str(iter['creator'])[1:-1].strip()
+                print(creator)
+                ans = '作者：' + creator
+                if jp_name:
+                    ans += '\n 本子名称：' + jp_name.strip()
+                else:
+                    ans += '\n 本子名称：' + eng_name.strip()
+                ans += '\n' + "[CQ:image,file=" + thum + ",cache=1]"
+                await bot.send(event, ans)
+            elif iter['type'] == 'pic':
+                continue
+                pid = iter['pid']
+                author = iter['author']
+                img = setuMesg(iter['url'])
+                ans = '作者：' + author + '\npixiv id：' + pid + '\n相似度：' + simp + '\n'
+                await bot.send(event, ans)
+                await bot.send(event, img)
--- plugins/utils.py
+++ plugins/utils.py
@@ -1,8 +1,11 @@
 from tokenize import String
 from nonebot import on_command, CommandSession, MessageSegment
 import requests
+from random import choice
 from bs4 import BeautifulSoup
 import re
+
+from sklearn.feature_extraction import img_to_graph
 
 URL = r"https://api.lolicon.app/setu/v2?"
 HEADER = {
@@ -11,6 +14,9 @@ HEADER = {
 
 # setu
 def getSetuInfo(keyword: str):
+    '''
+    从消息中提取涩图tag并获取涩图的info
+    '''
     if keyword[0]=='!':
         keyword = keyword[1:-2]
     else:
@@ -58,6 +64,9 @@ def pidGetCatUrl(pid):
     pass
 
 def setuMesg(setu_url: str):
+    '''
+    传入图片url得到可以通过bot.send()发送的MessageSegment
+    '''
     # pic
     # imgurl = js['data'][0]['urls']['original'].replace('cat', 're', 1)
     # await session.send("[CQ:image,file=" + imgurl + ",cache=1]")
@@ -68,16 +77,74 @@ def setuMesg(setu_url: str):
 # saucenao
 
 def getCQimgInMesg(mesg: str):
+    '''
+    把原始消息中所有图片的CQ码分离出来
+
+    返回CQ码的list
+    '''
     mesg = ''.join(mesg.strip().split(' '))
     CQimg = re.findall('\[CQ:image[^\]]*', mesg)
     return CQimg
 
 def getImgUrlInCQ(CQ: list):
+    '''
+        需要满足
+        [CQ:image,
+        file=xxxx,
+        url=xxxx,subType=x]
+        这种格式
+
+        返回url=后面除去]的所有内容
+    '''
     ans = []
     for mesg in CQ:
         url = mesg[mesg.find('url') + 4:]
         ans.append(url)
     return ans
 
-def saucenaoSearch(urllist):
-    pass
+def saucenaoSearch(urllist: list, SAUCEURL: str):
+    '''
+    传入图片list及SAUCE已经拼接好参数的url进行图片检索
+    返回图片检索得到的一些基本信息list
+    index_id表：https://saucenao.com/tools/examples/api/index_details.txt
+    可以查搜索得到的type，没写
+    '''
+    info = []
+    for img in urllist:
+        #print("\n\n\n")
+        try:
+            data = requests.get(SAUCEURL + img, timeout=5).json()
+            #print(data["results"])
+        except:
+            return None
+        res = data.get("results", "result")
+
+        for i in range(3):
+            data = res[i]
+            #print("\n\n\n")
+            #print(data)
+            similarity = data["header"]["similarity"]
+            thumbnail = data["header"]["thumbnail"] # 缩略图 : url
+
+            if float(similarity) >= 70:
+                tmp = {}
+                tmp["similarity"] = similarity
+                tmp["thumbnail"] = thumbnail
+                tmp["index_name"] = data["header"]["index_name"]
+                tmp["url"] = choice(data["data"].get("ext_urls", ["None"]))
+                tmp["creator"] = data["data"].get('creator')
+                tmp["eng_name"] = data["data"].get('eng_name')
+                tmp["jp_name"] = data["data"].get('jp_name')
+                tmp["type"] = "pic"
+                if tmp["eng_name"] or tmp["jp_name"]:
+                    tmp["type"] = "doujin" 
+                info.append(tmp)
+    print("\n\n\n")
+    print(info)
+    print("\n\n\n")
+    return info
+
+        
+
+
+
